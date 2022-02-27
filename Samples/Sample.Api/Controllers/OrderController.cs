9fef1ebd18f99498f12d36e021c8e96d69dcfc52
LAPTOP-A3I50TBS\nsontung <nsontung.it@gmail.com>
2022-02-27 11:11:44 +0700

    Mediator

---
+++ Samples/Sample.Api/Controllers/OrderController.cs
@@ -0,0 +1,49 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Sample.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        readonly ILogger<OrderController> _logger;
+        readonly IRequestClient<SubmitOrder> _submitOrderRequestClient;
+
+        public OrderController(ILogger<OrderController> logger
+            , IRequestClient<SubmitOrder> submitOrderRequestClient)
+        {
+            _logger = logger;
+            _submitOrderRequestClient = submitOrderRequestClient;
+        }
+        [HttpPost]
+        public async Task<ActionResult> Post(Guid id, string customerNumber)
+        {
+            var (accepted, rejected) = await _submitOrderRequestClient.GetResponse<OrderSubmissionAccepted, OrderSubmissionRejected>(new
+            {
+                // it mean this below object is a context is sent to Consumer, so its type is SubmitOrder
+                // nghĩa là cái đối tượng truyền vào trong tham số bên dưới này, nó là context cho thằng Consumer ==> Nó có phải có kiểu là SubmitOrder
+                OrderId = id,
+                InVar.Timestamp,
+                CustomerNumber = customerNumber
+            });
+            if (accepted.IsCompletedSuccessfully)
+            {
+                var response = await accepted;
+                return Accepted(response.Message);
+            }
+            else
+            {
+                var response = await rejected;
+                return BadRequest(response.Message);
+            }
+        }
+    }
+}
---
+++ Samples/Sample.Components/Consumers/SubmitOrderConsumer.cs
@@ -0,0 +1,44 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Sample.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Components.Consumers
+{
+    public class SubmitOrderConsumer : IConsumer<SubmitOrder>
+    {
+        readonly ILogger<SubmitOrderConsumer> _logger;
+
+        public SubmitOrderConsumer(ILogger<SubmitOrderConsumer> logger)
+        {
+            _logger = logger;
+        }
+        public async Task Consume(ConsumeContext<SubmitOrder> context)
+        {
+            _logger.LogDebug($"SubmitOrderConsumer: {context.Message.CustomerNumber}");
+            if (context.Message.CustomerNumber.Contains("TEST"))
+            {
+                await context.RespondAsync<OrderSubmissionRejected>(new
+                {
+
+                    Reason = "CustomerNumber was invalid",
+                    InVar.Timestamp,
+                    OrderId = context.Message.OrderId,
+                    CustomerNumber = context.Message.CustomerNumber
+                });
+
+                return;
+            }
+            await context.RespondAsync<OrderSubmissionAccepted>(new
+            {
+                InVar.Timestamp,
+                OrderId = context.Message.OrderId,
+                CustomerNumber = context.Message.CustomerNumber
+            });
+        }
+    }
+}
